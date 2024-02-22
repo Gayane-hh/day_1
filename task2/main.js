b264139c59a4e28c64cae9d9a3e4fd86dfba72ca
Gayane-hh <139545787+Gayane-hh@users.noreply.github.com>
2024-02-23 00:47:12 +0400

    Add files via upload

---
+++ task2/main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const paragraph = document.getElementById("p");
+
+btn.addEventListener("click", function(){
+    paragraph.innerText = "welcome my page";
+
+});
